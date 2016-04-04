5a75451037558da5a7e02d990dff47029a6dd0a7
Greg Wiley <aztec.rex@jammm.com>
2016-04-03 19:49:32 -0700

    react

---
+++ app/components/App.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import Note from './Note.jsx';
+
+export default () => <Note />;
--- app/index.js
+++
@@ -1,7 +0,0 @@
-const component = require('./component');
-const app = document.createElement('div');
-require('./main.css');
-
-document.body.appendChild(app);
-
-app.appendChild(component());
